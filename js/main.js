e0285bbc84fc0956b165c1eb7ba0e4be17ced2f7
michael-cwm <michaelcwm@hotmail.com>
2021-11-04 16:18:57 +0100

    merged feature-list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 window.onload = function (){
-    
+    start();
 }
+
+function start(){
+let myList = document.getElementsByTagName("li");
+
+
+for(let i = 0; i < myList.length; i++) {
+    let span = document.createElement("span");
+    let textNode = document.createTextNode("\u2573");
+    span.className = "closeButton";
+    span.appendChild(textNode);
+    myList[i].appendChild(span);
+}
+
+let closeButton = document.getElementsByClassName("closeButton");
+
+for(let i = 0; i < closeButton.length; i++){
+    closeButton[i].onclick = function(){
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+}
+
