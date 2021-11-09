cbbc0890ed7d79712812df9e214c5d52f6872a7e
michael-cwm <michaelcwm@hotmail.com>
2021-11-09 09:23:22 +0100

    added functionality for add new

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = function (){
 function start(){
 let myList = document.getElementsByTagName("li");
 
-
+// Lägger till en close-button för varje list-item
 for(let i = 0; i < myList.length; i++) {
     let span = document.createElement("span");
     let textNode = document.createTextNode("\u2573");
@@ -16,6 +16,7 @@ for(let i = 0; i < myList.length; i++) {
 
 let closeButton = document.getElementsByClassName("closeButton");
 
+// Tar bort berört list-item vid tryck på close-button
 for(let i = 0; i < closeButton.length; i++){
     closeButton[i].onclick = function(){
         let div = this.parentElement;
@@ -25,6 +26,7 @@ for(let i = 0; i < closeButton.length; i++){
 
 }
 
+// Stryker över och ändrar färg på berört list-item
 let checkedColor = "lightgreen";
 
 function done(e){
@@ -39,7 +41,32 @@ function revert(e){
     e.setAttribute("onclick", "done(this)");
 }
 
-// newListItem(){
-//     let li = document.createElement("li");
-//     let inputValue = document.getElementById("myInput");
-// }
+let closeButton = document.getElementsByClassName("closeButton");
+
+// Lägger till ny list-item i listan samt funktionalitet för att ta bort och markera som klart
+function newListItem(){
+    let li = document.createElement("li");
+    li.setAttribute("onclick", "done(this)");
+    let inputValue = document.getElementById("myInput").value;
+    let text = document.createTextNode(inputValue);
+    li.appendChild(text);
+    document.getElementById("theList").appendChild(li);
+
+    let span = document.createElement("span");
+    let textNode = document.createTextNode("\u2573");
+    span.className = "closeButton";
+    span.appendChild(textNode);
+    li.appendChild(span);
+
+    for(let i = 0; i < closeButton.length; i++){
+        closeButton[i].onclick = function(){
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+
+    
+
+
+
+}
